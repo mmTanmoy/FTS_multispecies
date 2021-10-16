abda303eacbb779811659197bd387b1670367cc3
mmTanmoy <60401332+mmTanmoy@users.noreply.github.com>
2021-10-16 15:30:08 -0400

    Update calc_correlations.py

--- calc_correlations.py
+++ calc_correlations.py
@@ -1,13 +1,26 @@
+#############################################################################
+# The following script could be used to compute Pair Distribution Functions #
+# (PDFs) from the generated field and density snapshots. It uses pandas     #
+# along with numpy. For an 'n' number of indepent runs with same lB and     #
+# polymer bulk densities, it out puts the file
+#            'svX-svY_correlations_cross_self0_self1.txt'                   #
+# in which the columns are (r, mean-of-Cross.Re, std-of-Cross.Re,           #
+# mean-of-SelfX.Re, std-of-SelfX.Re, and mean-of-SelfY.Re, std-of-SelfY.Re).#
+#############################################################################
+
 import numpy as np
 import sys
 from pathlib import Path
+import pandas as pd
 
+# The following parameter values must be same as used in
+# FTS_polyampholytes_multispecies.py
 Nx = 32
 a = np.sqrt(1./6.)
 dx = a
 V = (Nx*dx)**3.
 dv = dx*dx*dx
-u0 = 0.068
+v = 0.068
 
 def ft(x):
     return np.fft.fftn(x) * dv
@@ -16,56 +29,73 @@ def ift(x):
 
 def cal_correlations(w, rho):
 
-    w = ft(w)
+    ft_w = ft(w)
     rho0_p = ft(rho[0])
-    rho0_c = np.conjugate(ft(np.conjugate(rho[0])))
-    rho1_p = ft(rho[1])
-    rho1_c = np.conjugate(ft(np.conjugate(rho[1])))
+    rho0_c = np.conjugate(ft(np.conjugate(rho[0]))) # FT with -Ve wave vectors
+    #rho1_p = ft(rho[1])
+    rho1_c = np.conjugate(ft(np.conjugate(rho[1]))) # FT with -Ve wave vectors
     
     cross = rho0_p * rho1_c
-    auto0 = (1J/u0) * w * rho0_c - cross
-    auto1 = (1J/u0) * w * rho1_c - cross
+    self0 = (1J/v) * ft_w * rho0_c - cross
+    self1 = (1J/v) * ft_w * rho1_c - cross
 
     cross = ift(cross)/V
-    auto0 = ift(auto0)/V
-    auto1 = ift(auto1)/V
+    self0 = ift(self0)/V
+    self1 = ift(self1)/V
     
-    return [cross, auto0, auto1]
+    return [cross, self0, self1]
 
+# Listing all the relative distances for the periodic box
+rsq = np.array([[[np.minimum(i, Nx-i)**2 + np.minimum(j, Nx-j)**2 + \
+                  np.minimum(k, Nx-k)**2 for i in range(Nx)] for j \
+                 in range(Nx)] for k in range(Nx)])
+rs = np.sqrt(rsq)*dx
+rs = rs.flatten()
 
-cross_avg = np.zeros((Nx,Nx,Nx), dtype=complex)
-auto0_avg = np.zeros((Nx,Nx,Nx), dtype=complex)
-auto1_avg = np.zeros((Nx,Nx,Nx), dtype=complex)
-samples = 0.
+runs = 40               # Number of independent runs
+df_all = pd.DataFrame()
+starting_step = 20000   # Initial CL-step after equilibration to start sampling
+ending_step   = 54001   # Ending CL-step of sampling
+interval      = 500     # Sampling interval
+for run in range(runs):
 
-for cl_step in range(20000, 54001, 500):
+    cross_avg = np.zeros((Nx,Nx,Nx), dtype=complex)
+    self0_avg = np.zeros((Nx,Nx,Nx), dtype=complex)
+    self1_avg = np.zeros((Nx,Nx,Nx), dtype=complex)
+    samples = 0.
 
-    fname = '../densities/density_sv28_sv9_'+sys.argv[1]+'_step-'+str(cl_step)+'.npy'
-    if Path(fname).is_file():
-        print(fname)
-        with open(fname, 'rb') as fs:
-            w = np.load(fs)
-            psi = np.load(fs)
-            rho = np.load(fs)
+    for cl_step in range(starting_step, ending_step, interval):
+
+        fname = '../densities/density_sv28_sv24_' + str(run) + \
+            '_step-' + str(cl_step) + '.npy'
+        if Path(fname).is_file():
+            print(fname)
+            with open(fname, 'rb') as fs:
+                w = np.load(fs)
+                psi = np.load(fs)
+                rho = np.load(fs)
         
-        samples += 1.
-        [cross, auto0, auto1] = cal_correlations(w, rho)
+            samples += 1.
+            [cross, self0, self1] = cal_correlations(w, rho)
 
-        cross_avg += cross
-        auto0_avg += auto0
-        auto1_avg += auto1
+            cross_avg += cross
+            self0_avg += self0
+            self1_avg += self1
 
-cross_avg /= samples
-auto0_avg /= samples
-auto1_avg /= samples
+    cross_avg /= samples
+    self0_avg /= samples
+    self1_avg /= samples
+    
+    frame = { 'r' : rs, 'cross' : cross_avg.real.flatten(),
+              'self0' : self0_avg.real.flatten(),
+              'self1' : self1_avg.real.flatten()}
+    df = pd.DataFrame(frame)
+    grouped_by_r = df.groupby('r', as_index=False).mean()
+    df_all = df_all.append(grouped_by_r, ignore_index=True)
 
-# saving to .txt files after corroborating periodic boundary conditions
-f = open('correlations_run-'+sys.argv[1]+'.txt', 'w')
-for i in range(Nx):
-    for j in range(Nx):
-        for k in range(Nx):
-            r = np.sqrt(np.minimum(i, Nx-i)**2. + np.minimum(j, Nx-j)**2. + np.minimum(k, Nx-k)**2.)*dx
-            np.savetxt(f, np.array([ r, cross_avg[i,j,k], auto0_avg[i,j,k], auto1_avg[i,j,k] ]).real, delimiter="  ", fmt='%10.5f', newline=' ')
-            f.write('\n')
+mean_std = df_all.groupby('r').agg({ 'cross':['mean', 'std'],
+                                     'self0':['mean', 'std'],
+                                     'self1':['mean', 'std'] })
 
-f.close()
+outfile = 'sv28-sv24_correlations_cross_self0_self1.txt'
+mean_std.to_csv(outfile, sep='\t', index=True, na_rep='nan', header=False)
